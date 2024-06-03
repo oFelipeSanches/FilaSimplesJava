12a64e9ac60fc19dbea47a093b74ab092501f035
Felipe Sanches <felipesanches@edu.unifil.br>
2024-06-03 16:04:52 -0300

    versao-final

--- src/main/java/org/example/Executor.java
+++ src/main/java/org/example/Executor.java
@@ -5,26 +5,110 @@ public class Executor {
 
         FilaSimples<Integer> fila = new FilaSimples<>();
 
-        fila.enfileira(10);
-        fila.enfileira(10);
-        fila.enfileira(10);
-        fila.enfileira(10);
-        fila.enfileira(10);
-        System.out.println(fila.media());
-        System.out.println(fila.toString());
-        fila.removerTodasOcorrencias(1);
-        System.out.println(fila.toString());
-        fila.aumentarCapacidade();
-        fila.enfileira(2);
-        fila.enfileira(1);
-        fila.enfileira(7);
-        fila.enfileira(2);
-        fila.enfileira(4);
-        System.out.println(fila.toString());
-        fila.removerTodasOcorrencias(7);
-        System.out.println(fila.toString());
-        System.out.println(fila.media());
-        System.out.println(fila.buscar(4));
+        //enfileirar/adicionar
+//        fila.enfileira(1);
+//        fila.enfileira(2);
+//        fila.enfileira(3);
+//        fila.enfileira(4);
+//        fila.enfileira(5);
+//        System.out.println(fila.toString());
+
+        //desenfileira/remover
+//        fila.enfileira(1);
+//        fila.enfileira(2);
+//        fila.enfileira(3);
+//        fila.enfileira(4);
+//        fila.enfileira(5);
+//        System.out.println(fila.toString());
+//        fila.desenfileira();
+//        System.out.println(fila.toString());
+//        fila.desenfileira();
+//        System.out.println(fila.toString());
+//        fila.desenfileira();
+//        System.out.println(fila.toString());
+//        fila.desenfileira();
+//        System.out.println(fila.toString());
+//        fila.desenfileira();
+//        System.out.println(fila.toString());
+
+        //remover todas as ocorrencias
+//        fila.enfileira(4);
+//        fila.enfileira(5);
+//        fila.enfileira(3);
+//        fila.enfileira(4);
+//        fila.enfileira(5);
+//        System.out.println(fila.toString());
+//        fila.removerTodasOcorrencias(5);
+//        System.out.println(fila.toString());
+//        fila.removerTodasOcorrencias(4);
+//        System.out.println(fila.toString());
+//        fila.removerTodasOcorrencias(3);
+//        System.out.println(fila.toString());
+
+        //buscar
+//        fila.enfileira(1);
+//        fila.enfileira(2);
+//        fila.enfileira(3);
+//        fila.enfileira(4);
+//        fila.enfileira(5);
+//        System.out.println(fila.toString());
+//        System.out.println(fila.buscar(3));
+//        System.out.println(fila.toString());
+//        System.out.println(fila.buscar(7));
+
+        //maior
+//        fila.enfileira(1);
+//        fila.enfileira(2);
+//        fila.enfileira(5);
+//        fila.enfileira(4);
+//        fila.enfileira(3);
+//        System.out.println(fila.toString());
+//        System.out.println(fila.maior());
+
+        //menor
+//        fila.enfileira(3);
+//        fila.enfileira(2);
+//        fila.enfileira(1);
+//        fila.enfileira(4);
+//        fila.enfileira(5);
+//        System.out.println(fila.toString());
+//        System.out.println(fila.menor());
+
+//        //media
+//        fila.enfileira(1);
+//        fila.enfileira(2);
+//        fila.enfileira(3);
+//        fila.enfileira(4);
+//        fila.enfileira(5);
+//        System.out.println(fila.toString());
+//        System.out.println(fila.media());
+
+        //quantidade
+//        fila.enfileira(3);
+//        fila.enfileira(2);
+//        fila.enfileira(3);
+//        fila.enfileira(4);
+//        fila.enfileira(5);
+//        System.out.println(fila.toString());
+//        System.out.println(fila.quantidadeDeOcorrencias(3));
+
+        //aumentar capacidade
+//        fila.enfileira(1);
+//        fila.enfileira(2);
+//        fila.enfileira(3);
+//        fila.enfileira(4);
+//        fila.enfileira(5);
+//        System.out.println(fila.toString());
+//        System.out.println(fila.getTamanho());
+//        fila.aumentarCapacidade();
+//        fila.enfileira(6);
+//        fila.enfileira(7);
+//        fila.enfileira(8);
+//        fila.enfileira(9);
+//        fila.enfileira(10);
+//        System.out.println(fila.toString());
+//        System.out.println(fila.getTamanho());
+
 
     }
 }
